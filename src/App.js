60bf901f07926174c9d00771861c772fce7b0679
Parv <kulparv@gmail.com>
2022-07-04 10:55:01 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,18 +2,50 @@ import './App.css';
 import {Row} from "./components/Row"
 import requests from './requests';
 
+const Row_Type = [
+  {
+    title:"Trending",
+    fetchUrl:requests.fetchTrending
+  },
+  {
+    title:"Originals",
+    fetchUrl:requests.fetchNetflixOriginals
+  },
+  {
+    title:"Loved",
+    fetchUrl:requests.fetchTopRated
+  },
+  {
+    title:"Action",
+    fetchUrl:requests.fetchActionMovies
+  },
+  {
+    title:"Comedy",
+    fetchUrl:requests.fetchComedyMovies
+  },
+  {
+    title:"Horror",
+    fetchUrl:requests.fetchHorrorMovies
+  },
+  {
+    title:"Romance",
+    fetchUrl:requests.fetchRomanceMovies
+  },
+  {
+    title:"Documenteries",
+    fetchUrl:requests.fetchDocumantaries
+  },
+]
+
 function App() {
+  const content = Row_Type.map((genre,index) => {
+    return(
+    <Row title={genre.title} fetchUrl={genre.fetchUrl} key={index} />)
+  })
   return (
     <div className="App">
       <div className='logo'>Mflix</div>
-      <Row title="Trending" fetchUrl={requests.fetchTrending} />
-      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} />
-      <Row title="Most Loved" fetchUrl={requests.fetchTopRated} />
-      <Row title="Action" fetchUrl={requests.fetchActionMovies} />
-      <Row title="Comedy" fetchUrl={requests.fetchComedyMovies} />
-      <Row title="Horror" fetchUrl={requests.fetchHorrorMovies} />
-      <Row title="Romance" fetchUrl={requests.fetchRomanceMovies} />
-      <Row title="Documenteries" fetchUrl={requests.fetchDocumantaries} />
+      {content}
     </div>
   );
 }
