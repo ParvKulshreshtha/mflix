46944693ddf6e7f9a5aa78d6cc11bf812fd28a47
Parv <kulparv@gmail.com>
2022-07-06 14:20:55 +0530

    commit

--- src/requests.js
+++ src/requests.js
@@ -1,15 +1,14 @@
-const API_KEY ="6d6cdb3c698b682f98ecf65b7e47f201"
-
+const apiKey = process.env.REACT_APP_API_KEY
+console.log(apiKey)
 const requests = {
-    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-    // /discover/tv?api_key=6d6cdb3c698b682f98ecf65b7e47f201&with_networks=213
-    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
-    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
-    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-    fetchDocumantaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`
+    fetchTrending: `/trending/all/week?api_key=${apiKey}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${apiKey}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${apiKey}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${apiKey}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${apiKey}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${apiKey}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${apiKey}&with_genres=10749`,
+    fetchDocumantaries: `/discover/movie?api_key=${apiKey}&with_genres=99`
   }
 
   export default requests;
